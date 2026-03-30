0d6327015f88e837c800c49a6ec6fb47f4af4bf9
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-03-30 20:49:09 +0600

    added

---
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FamilyTree = () => {
+    return (
+        <div>
+            <h3>Famiy Tree
+                
+            </h3>
+        </div>
+    );
+};
+
+export default FamilyTree;
